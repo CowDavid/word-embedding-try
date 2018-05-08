eb5c8e596914042ea554f1281998d1ec1173a07c
CowDavid <31068867+CowDavid@users.noreply.github.com>
2018-05-09 01:27:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import argparse
 from train import trainIters
-from evaluate import runTest
+from evaluate import runTest, loss_graph
 from load import Voc
-
 
 def parse():
     parser = argparse.ArgumentParser(description='Attention Seq2Seq Chatbot')
@@ -22,6 +21,8 @@ def parse():
     parser.add_argument('-lr', '--learning_rate', type=float, default=0.01, help='Learning rate')
     parser.add_argument('-s', '--save', type=float, default=10000, help='Save every s iterations')
     parser.add_argument('-pre', '--pretrained_model', help='Pretrained tri-gram model')
+    parser.add_argument('-lo', '--loss', help='Draw the loss trend graph while the seq2seq model was being trained')
+    parser.add_argument('-dc', '--diff_corpus', help='Different corpus for testing')
     args = parser.parse_args()
     return args
 
@@ -48,7 +49,10 @@ def run(args):
                     n_layers, hidden_size, print_every, save_every, loadFilename=args.load)
     elif args.test:
         n_layers, hidden_size, reverse = parseFilename(args.test, True)
-        runTest(n_layers, hidden_size, reverse, args.test, beam_size, input, args.corpus)
+        runTest(n_layers, args.pretrained_model, hidden_size, reverse, args.test, beam_size, input, args.corpus, args.diff_corpus)
+    elif args.loss:
+        loss_graph(args.loss, args.corpus, hidden_size)
+
 
 
 if __name__ == '__main__':
